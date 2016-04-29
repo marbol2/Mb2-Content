73904dc2bd469a151c4e2d4d6f48ce16398da2c8
marbol2 <mariuszboloz@gmail.com>
2016-04-29 20:24:24 +0200

    Added files via upload

---
+++ media/js/mb2content.js
@@ -0,0 +1,8 @@
+/**
+ * @package		Mb2 Content
+ * @version		1.5.0
+ * @author		Mariusz Boloz (http://mb2extensions.com)
+ * @copyright	Copyright (C) 2013 - 2016 Mariusz Boloz (http://mb2extensions.com). All rights reserved
+ * @license		GNU/GPL (http://www.gnu.org/copyleft/gpl.html)
+**/
+!function(a){a(window).load(function(){a(".mb2-content-list").each(function(){var t=a(this);if(t.hasClass("is-carousel")){var i=t.data("items"),n=t.data("move"),e=t.data("margin"),o=(t.data("anim"),t.data("anim_speed")),s=t.data("anim_pause"),c=t.data("auto"),d=t.data("loop"),r=t.data("dirnav"),l=t.data("cnav");isDnav=1==r?!0:!1,isCnav=1==l?!0:!1,isAuto=1==c?!0:!1,isLoop=1==d?!0:!1,t.lightSlider({item:i,slideMargin:e,slideMove:n,speed:o,auto:isAuto,loop:isLoop,pause:s,controls:isDnav,pager:isCnav,vertical:!1,prevHtml:'<span class="mb2c_icon-left-open-big"></span>',nextHtml:'<span class="mb2c_icon-right-open-big"></span>',responsive:[{breakpoint:768,settings:{item:i>=2?2:1}},{breakpoint:600,settings:{item:1,slideMove:1}}],keyPress:!0})}})})}(jQuery),jQuery(document).ready(function(a){a(".mb2-content").each(function(){var t=a(this),i=t.data("lbx");1==i&&t.on("mouseover",function(){a(".mb2-content-lightbox").nivoLightbox()})})});
---
+++ media/js/mb2content.source.js
@@ -0,0 +1,164 @@
+/**
+ * @package		Mb2 Content
+ * @version		1.5.0
+ * @author		Mariusz Boloz (http://mb2extensions.com)
+ * @copyright	Copyright (C) 2013 - 2016 Mariusz Boloz (http://mb2extensions.com). All rights reserved
+ * @license		GNU/GPL (http://www.gnu.org/copyleft/gpl.html)
+**/
+
+
+
+
+
+
+
+(function($){$(window).load(function(){	
+
+	
+	
+	
+	
+	$('.mb2-content-list').each(function(){		
+		
+		
+		var carouselList = $(this);
+		
+		
+		if (carouselList.hasClass('is-carousel'))
+		{
+			
+			
+			
+			//carouselList.hassClass('lightSlider')
+//			{
+//				carouselList.show();	
+//			}
+			
+			
+			
+			
+			var itemsCount = carouselList.data('items');
+			var itemsMove = carouselList.data('move');
+			var itemsMargin = carouselList.data('margin');
+			var itemsAnim = carouselList.data('anim');
+			var itemsAnimSpeed = carouselList.data('anim_speed');
+			var itemsAnimPause = carouselList.data('anim_pause');
+			var itemsAnimAuto = carouselList.data('auto');
+			var itemsLoop = carouselList.data('loop');			
+			var itemsDnav = carouselList.data('dirnav');
+			var itemsCnav = carouselList.data('cnav');
+			
+			
+			isDnav = itemsDnav == 1 ? true : false;
+			isCnav = itemsCnav == 1 ? true : false;
+			isAuto = itemsAnimAuto == 1 ? true : false;
+			isLoop = itemsLoop == 1 ? true : false;
+			
+			
+			
+			
+			carouselList.lightSlider({
+				
+				item: itemsCount,
+				slideMargin: itemsMargin,
+				slideMove: itemsMove,
+				speed: itemsAnimSpeed, 
+				auto: isAuto,
+				loop: isLoop,
+				pause: itemsAnimPause,
+				controls: isDnav,
+				pager: isCnav,
+				vertical: false,
+				prevHtml: '<span class="mb2c_icon-left-open-big"></span>',
+				nextHtml: '<span class="mb2c_icon-right-open-big"></span>',
+				responsive : [
+					{
+						breakpoint:768,
+						settings: {
+							item: itemsCount>=2 ? 2 : 1
+						  }
+					},
+					{
+						breakpoint:600,
+						settings: {
+							item:1,
+							slideMove:1
+						  }
+					}
+				],
+				keyPress: true
+				
+			}); 
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+	});
+	
+	
+	
+	
+
+
+
+})})(jQuery);//end
+
+
+
+
+
+
+
+
+
+
+jQuery(document).ready(function($){
+	
+	
+	
+	
+	
+	
+	$('.mb2-content').each(function(){	
+	
+		
+		
+		var module = $(this);
+		var isLightbox = module.data('lbx');
+		
+		
+		if (isLightbox == 1)
+		{
+			
+			module.on('mouseover',function(){
+				
+				$('.mb2-content-lightbox').nivoLightbox();
+				
+			});
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+});
